61f51627e1824f573a0033730749f83b4d8badfe
Md Arman Hawlader <rodneylwindham@gmail.com>
2026-07-08 16:31:07 +0600

    Upgrade Firebase SDK to version 12.0.0

--- admin.js
+++ admin.js
@@ -1,49 +1,64 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import {
-getDatabase,
-ref,
-query,
-limitToLast,
-onValue
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+  getDatabase,
+  ref,
+  query,
+  limitToLast,
+  onValue,
+  push
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyAtO9e2IQKCwYjAC5PuvCFYcYozTWe9Oyk",
-authDomain: "monitijationhelper.firebaseapp.com",
-databaseURL: "https://monitijationhelper-default-rtdb.asia-southeast1.firebasedatabase.app",
-projectId: "monitijationhelper",
-storageBucket: "monitijationhelper.firebasestorage.app",
-messagingSenderId: "251665802546",
-appId: "1:251665802546:web:c41a366f15323b4bcaa32b"
+  apiKey: "AIzaSyAtO9e2IQKCwYjAC5PuvCFYcYozTWe9Oyk",
+  authDomain: "monitijationhelper.firebaseapp.com",
+  databaseURL: "https://monitijationhelper-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "monitijationhelper",
+  storageBucket: "monitijationhelper.firebasestorage.app",
+  messagingSenderId: "251665802546",
+  appId: "1:251665802546:web:c41a366f15323b4bcaa32b"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const messagesRef = query(
-ref(db, "messages"),
-limitToLast(10)
-);
+const messagesDiv = document.getElementById("messages");
 
-onValue(messagesRef, (snapshot) => {
+const messagesQuery = query(ref(db, "messages"), limitToLast(10));
 
-const box = document.getElementById("messages");
+onValue(messagesQuery, (snapshot) => {
 
-box.innerHTML = "";
+  messagesDiv.innerHTML = "";
 
-snapshot.forEach((child) => {
+  snapshot.forEach((child) => {
 
-const data = child.val();
+    const data = child.val();
 
-box.innerHTML += `
-<div class="msg">
-<p><b>Text:</b> ${data.text}</p>
-<p><b>Reference:</b> ${data.reference}</p>
-<hr>
-</div>
-`;
+    messagesDiv.innerHTML += `
+      <div style="background:#374151;color:white;padding:12px;border-radius:8px;margin-top:10px;">
+        <b>Message:</b><br>${data.text}<br><br>
+        <b>Reference:</b><br>${data.reference}
+      </div>
+    `;
+  });
 
 });
 
-});
+document.getElementById("replyBtn").onclick = function(){
+
+  const reply = document.getElementById("replyText").value;
+
+  if(reply==""){
+    alert("Reply লিখুন");
+    return;
+  }
+
+  push(ref(db,"replies"),{
+    text:reply,
+    time:Date.now()
+  });
+
+  document.getElementById("replyText").value="";
+
+  alert("Reply Sent");
+
+};
